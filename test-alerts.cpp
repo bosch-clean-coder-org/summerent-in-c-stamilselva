9f06ed84ad84a2a01e2d2a0031dc08793f1e0a16
stamilselva <139846830+stamilselva@users.noreply.github.com>
2023-07-18 13:17:54 +0530

    Update test-alerts.cpp
    Build errors resolved

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,7 +6,7 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
-  REQUIRE(inferBreach(25, 20, 30) == TOO_NORMAL);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
 }
 
 TEST_CASE("classify the Temperature Breach according to type of cooling") {
@@ -16,7 +16,7 @@ TEST_CASE("classify the Temperature Breach according to type of cooling") {
 }
 
 TEST_CASE("Check and alert temperature breach according to target") {
-  BatteryCharacter stBatChar={0};
+  BatteryCharacter stBatChar;
   stBatChar.coolingType = HI_ACTIVE_COOLING;
   checkAndAlert(TO_CONTROLLER, stBatChar, 50);
   checkAndAlert(TO_EMAIL, stBatChar, 50);
