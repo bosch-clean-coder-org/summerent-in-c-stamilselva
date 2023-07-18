41ff79bdf68ea2f134736467bd8bf743c8199280
stamilselva <139846830+stamilselva@users.noreply.github.com>
2023-07-18 13:10:13 +0530

    Update test-alerts.cpp
    UT code added for coverage improvement

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,20 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == TOO_NORMAL);
 }
+
+TEST_CASE("classify the Temperature Breach according to type of cooling") {
+  classifyTemperatureBreach(PASSIVE_COOLING, 20);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,46) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,30) == NORMAL);
+}
+
+TEST_CASE("Check and alert temperature breach according to target") {
+  BatteryCharacter stBatChar={0};
+  stBatChar.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER, stBatChar, 50);
+  checkAndAlert(TO_EMAIL, stBatChar, 50);
+}
+
